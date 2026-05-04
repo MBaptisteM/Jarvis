f15a98f574ce067bbf231e8f7fcfab13deb1798d
baptiste.mahe <baptiste.mahe@epita.fr>
2026-05-04 20:53:41 +0900

    subject

---
+++ subject/get_subject.js
@@ -0,0 +1,65 @@
+const { chromium } = require('playwright');
+const fs = require('fs');
+
+(async () => {
+  const mode = process.argv[2];
+  const url = process.argv[3];
+  const subject_path = process.argv[4];
+
+  if (!mode || !url || !subject_path) {
+    console.error("Usage: node get_subject.js <login|run> <url>");
+    process.exit(1);
+  }
+
+  const auth_path = subject_path + '/auth.json';
+
+  // Start the Auth window
+  if (mode === "login") {
+    console.log("\n➡️ \x1b[1mPlease \x1b[32mconnect to your forge account.\x1b[0m");
+    console.log("➡️ \x1b[1mThen close the window.\x1b[0m");
+
+    const browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto(url);
+
+    page.on('close', async () => {
+      await context.storageState({ path: auth_path });
+      console.log("Auth saved !");
+
+      await browser.close();
+      process.exit(0);
+    });
+  }
+
+  // Run and load the subject
+  else if (mode === "run") {
+    if (!fs.existsSync(auth_path)) {
+      console.error("ERROR : Impossible to find auth.json, please auth before");
+      process.exit(1);
+    }
+
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext({
+      storageState: auth_path
+    });
+
+    const page = await context.newPage();
+    await page.goto(url);
+
+    const content = await page.content();
+
+    // Write the content in subject.html
+    fs.writeFileSync('subject.html', content, 'utf-8');
+    console.log("Content saved in subject.html");
+
+    await browser.close();
+  }
+
+  else {
+    console.error("ERROR : Unknown mode :", mode);
+    process.exit(1);
+  }
+
+})();
