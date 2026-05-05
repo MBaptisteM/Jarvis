761cf0f37c04078b73086cc6434d9778da1eb523
baptiste.mahe <baptiste.mahe@epita.fr>
2026-05-05 23:32:35 +0900

    get_given_files

--- subject/get_subject.js
+++ subject/get_subject.js
@@ -7,7 +7,7 @@ const fs = require('fs');
   const subject_path = process.argv[4];
 
   if (!mode || !url || !subject_path) {
-    console.error("Usage: node get_subject.js <login|run> <url>");
+    console.error("Usage: node get_subject.js <login|run|download> <url>");
     process.exit(1);
   }
 
@@ -53,6 +53,56 @@ const fs = require('fs');
 
     // Write the content in subject.html
     fs.writeFileSync(subject_html_path, content, 'utf-8');
+
+    await browser.close();
+  }
+
+
+  // Download the file if it exist (call only with .gz pages)
+  else if (mode === "download") {
+    if (!fs.existsSync(auth_path)) {
+      console.error("ERROR Impossible to find auth.json, please auth before");
+      process.exit(1);
+    }
+
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext({
+      storageState: auth_path
+    });
+
+    const request = context.request;
+
+    try {
+      const response = await request.get(url);
+
+      // No file to download
+      if (response.status() === 404) {
+        console.error("No given file for this TP.");
+        await browser.close();
+        process.exit(1);
+      }
+
+      // HTTP error handling
+      if (!response.ok()) {
+        console.error(`HTTP ERROR : ${response.status()}`);
+        await browser.close();
+        process.exit(1);
+      }
+
+      // Get the content
+      const buffer = await response.body();
+
+      const fileName = url.split('/').pop() || 'file.gz';
+      const filePath = `${subject_path}/${fileName}`;
+
+      fs.writeFileSync(filePath, buffer);
+
+      console.log(`\x1b[32mGiven files downloaded.\x1b[0m`);
+
+    } catch (err) {
+      console.error("ERROR Impossible to get the given files :", err.message);
+      process.exit(1);
+    }
 
     await browser.close();
   }
