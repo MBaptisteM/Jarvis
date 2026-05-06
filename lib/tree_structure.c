290431a45c9dbad913252e2e4d6e3c4c3b760863
baptiste.mahe <baptiste.mahe@epita.fr>
2026-05-06 20:23:40 +0900

    tree_structure

---
+++ lib/tree_structure.c
@@ -0,0 +1,23 @@
+#include "tree_structure.h";
+
+
+int IsEmpty(char* path){
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(path);
+
+    if (dir == NULL) {
+        errx(-1, "ERROR Impossible to open folder %s", path);
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+            closedir(dir);
+            return 0;
+        }
+    }
+
+    closedir(dir);
+    return 1;
+}
---
+++ lib/tree_structure.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <err.h>
+
+
+int IsEmpty(char* path);
