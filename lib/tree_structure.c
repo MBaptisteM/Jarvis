60d350cde2d573ffd7ce7f791e156f93cb83bc8a
baptiste.mahe <baptiste.mahe@epita.fr>
2026-05-07 23:58:34 +0900

    CleanFolder

--- lib/tree_structure.c
+++ lib/tree_structure.c
@@ -12,7 +12,7 @@ int IsEmpty(char* path){
     }
 
     while ((entry = readdir(dir)) != NULL) {
-        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 && strcmp(entry->d_name, ".git") != 0){
             closedir(dir);
             return 0;
         }
@@ -20,4 +20,30 @@ int IsEmpty(char* path){
 
     closedir(dir);
     return 1;
+}
+
+int CleanFolder(char *folder_path){
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(folder_path);
+
+    if (dir == NULL) {
+        errx(-1, "ERROR Impossible to open the folder %s", folder_path);
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 && strcmp(entry->d_name, ".git") != 0){
+            char to_remove_path[1024];
+            snprintf(to_remove_path, 1024, "rm -rf %s/%s", folder_path, entry->d_name);
+            if (system(to_remove_path))
+                errx (EXIT_FAILURE, "ERROR Impossible ro remove %s/%s", folder_path, entry->d_name);
+        }
+    }
+
+    printf("Content of %s deleted successfully.\n", folder_path);
+
+    closedir(dir);
+
+    return EXIT_SUCCESS;
 }
--- lib/tree_structure.h
+++ lib/tree_structure.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 #include <err.h>
 
 
 int IsEmpty(char* path);
+int CleanFolder(char *folder_path);
